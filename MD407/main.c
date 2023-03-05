eb0a3ae4e6a8c313d2150d8f5bda5468a61e9a0c
chetran <114576865+chetran@users.noreply.github.com>
2023-03-05 23:55:49 +0100

    snake

--- MD407/main.c
+++ MD407/main.c
@@ -2,71 +2,82 @@
 
 #include "debug.h"
 #include "startup.h"
-#include <stdlib.h>
+#include <stdbool.h>
 
-/*
-	TIM6 is one of the simplest counter module MD407 have. It lacks I/O support and counts upwards, from 1 to 0xFFFF.
-	That means "*TIM6_ARR = x" sets the boundary of the random number.
-	The speed of the counter is based on the processors clock speed. 
-
-	Interrupts from periferal circuits is controlled by the NVIC module. It handles 81 different interrupts. 
-	In the vector table the vector number and its offset is listed. 
-
-	The bit that needs to be set is calculated like this:
-		x = vector_number
-		bit = vector_number - (32 * x)
-	This bit is then set on the second register of the module. 
-
-*/
-
-// In startup.c are functions defined that is built in for processor MD407.
 // Timer 6
+#define STK_CTRL (volatile unsigned int *) 0xE000E010 
+#define STK_LOAD (volatile unsigned int *) 0xE000E014 
+#define STK_VAL (volatile unsigned int *) 0xE000E018 
+#define GPIO_D 0x40020C00
+#define GPIO_MODER ((volatile unsigned int *)   (GPIO_D))
+#define GPIO_ODR_HIGH ((volatile unsigned char *)   (GPIO_D+0x15))
+#define GPIO_ODR_LOW ((volatile unsigned char *)   (GPIO_D+0x14))
+#define GPIO_IDR_HIGH ((volatile unsigned char *)   (GPIO_D+0x11))
+// For asciidisplay.c
+#define PORT_BASE 0x40021000 // Port E 
+#define portModer ((volatile unsigned int *)  PORT_BASE)
+#define portOtyper ((volatile unsigned short *) (PORT_BASE+0x4))
+#define portOspeedr ((volatile unsigned int *) (PORT_BASE+0x8))
+#define portPupdr ((volatile unsigned int *) (PORT_BASE+0xC))
+#define portIdrLow ((volatile unsigned char *) (PORT_BASE+0x10))
+#define portIdrHigh ((volatile unsigned char *) (PORT_BASE+0x11))
+#define portOdrLow ((volatile unsigned char *) (PORT_BASE+0x14))	
+#define portOdrHigh ((volatile unsigned char *) (PORT_BASE+0x15))
+#define B_E 0x40
+#define B_SELECT 4
+#define B_RW 2
+#define B_RS 1
+
 #define TIM6_CR1 ((volatile unsigned short *) 0x40001000)
 #define TIM6_CNT ((volatile unsigned short *) 0x40001024)
 #define TIM6_ARR ((volatile unsigned short *) 0x4000102C)
-#define TIM6_SR ((volatile unsigned short *) 0x40001010)
-#define TIM6_PSC ((volatile unsigned short *) 0x40001028)
-#define TIM6_DIER ((volatile unsigned short *) 0x4000100C)
 #define UDIS (1<<1)
 #define CEN (1<<0)
-#define UIF (1<<0)
-#define UIE (1<<0)
-#define OPM (1<<3)
 
-#define PORT_D 0x40020C00
-#define PORT_E 0x40021000
-#define D_MODER ((volatile unsigned int *) PORT_D)
-#define ODR_LOW ((volatile unsigned char *) (PORT_D+0x14))
-#define ODR_HIGH ((volatile unsigned char *) (PORT_D+0x15))
-#define E_ODR_LOW ((volatile unsigned char *) (PORT_E+0x14))
-#define E_IDR_LOW ((volatile unsigned char *) (PORT_E+0x10))
+#define MAX_POINTS 50
+#define X_SIZE 128
+#define Y_SIZE 64
 
-#define NVIC_TIM6_ISER1 ((volatile unsigned int *) 0xE000E100)  
-#define NVIC_TIM6_ISER2 ((volatile unsigned int *) 0xE000E104) // second register of NIVC module 
-#define NVIC_TIM6_IRQ_BPOS (1<<22) // bit set to activate the timer. 
-#define TIM6_IRQVEC ((void (**)(void)) 0x2001C118)
-
-#define EXTI3_IRQVEC ((void (**)(void)) 0x2001C064)
-#define SYSCFG_EXTICR1 0x40013808
-#define EXTI_SWIER ((volatile unsigned int *) 0x40013C10)
-#define EXTI_PR ((volatile unsigned int *) 0x40013C14)
-
-
-// Functions
-void init_app(void);
-void exti3_handler(void);
-
-// Variables 
-volatile int count = 0;
-
-void main(void)
+// ------------------------------------------------------- STRUCTS ------------------------------------------------------------------------------- //
+typedef struct 
 {
-	init_app();
-	while(1)
-	{
-		*ODR_LOW = count;
-	}
-}
+	unsigned char x,y;
+} POINT, *PPOINT;
+
+typedef struct
+{
+	int numpoints;
+	int sizex;
+	int sizey;
+	POINT px[MAX_POINTS];
+} GEOMETRY, *PGEOMETRY;
+
+typedef struct tObj
+{
+	PGEOMETRY geo;
+	int posx, posy;
+	void (* draw) (struct tObj *);
+	void (* clear) (struct tObj *);
+	void (* move) (struct tObj *);
+	void (* set_speed) (struct tObj *, int, int);
+} OBJECT, *POBJECT;
+
+typedef struct {
+    int x, y;
+} apple_t;
+
+typedef enum {
+    UP, RIGHT, DOWN, LEFT
+} dir_t;
+
+// The snake
+typedef struct {
+    OBJECT body_part[X_SIZE * Y_SIZE];
+    int length;
+    dir_t dir;
+} snake_t;
+
+// ------------------------------------------------------- FUNCTIONS ------------------------------------------------------------------------------- //
 
 void init_app(void)
 {
@@ -78,45 +89,280 @@ void init_app(void)
 	* ((unsigned long *)0xE000ED08) = 0x2001C000;
 
 	// Initialize port D for display usage
-	*D_MODER = 0x55555555;	
+	*GPIO_MODER = 0x55555555;	
 
-	// Connect PE3 to EXTI3
-	*((unsigned int *) SYSCFG_EXTICR1) &= ~0xF000;
-	*((unsigned int *) SYSCFG_EXTICR1) |= 0x4000; // Configures PE3 to EXTI3
-
-	*((unsigned int *) 0x40013C00) |= 8; // Activates EXTI3
-	*((unsigned int *) 0x40013C08) |= 8; // Activates for rising flag
-	*((unsigned int *) 0x40013C0C) &= ~8; // Masks negative flag
-
-	*EXTI3_IRQVEC = exti3_handler; // sets up the interrupt handler
-
-	*NVIC_TIM6_ISER1 |= (1<<9);
-	
 }
 
-void exti3_handler(void)
+void timer6_init(void)
 {
-	if (*EXTI_PR & 8)
+	*TIM6_CR1 &= ~CEN; // Stops the counter module
+	*TIM6_ARR = 0xFFFF; // If its set to a low number, the random number will always be the upper bound because it counts to fast.
+	*TIM6_CR1 |= ( CEN | UDIS); // Activates the counter module and disables "update event"
+}
+
+// ------------------------------------------------------- DELAYS ------------------------------------------------------------------------------- //
+void delay_250ns(void)
+{
+	// page 98
+	*STK_CTRL = 0; // Resets SysTick
+	*STK_LOAD = (168 / 4) - 1; // Minus 1 comes from how to processor counts 
+	*STK_VAL = 0; // Resets counter register 
+	*STK_CTRL = 5; // Starts the count down 
+	while(*STK_CTRL & 0x00010000){} // while the countflag is 1 
+	*STK_CTRL = 0; // resets Systick 
+}
+
+void delay_micro(unsigned int ms)
+{
+	// When SIMULATOR is defined for some reason it gets stuck here. 	
+	#ifdef SIMULATOR
+		ms = ms / 1000;
+		ms++;
+	#endif
+	// 4 * 250 ns = 1 µs
+	for (int i = 0; i < ms; i++)
 	{
-		*EXTI_PR |= 8;
-		if (*E_IDR_LOW & 1)
-		{
-			*E_ODR_LOW |= (1<<4);
-			count++;
-		}
-		if (*E_IDR_LOW & 2)
-		{
-			*E_ODR_LOW |= (1<<5);
-			count = 0;
-		}
-		if (*E_IDR_LOW & 4)
-		{
-			*E_ODR_LOW |= (1<<6);
-			if (*ODR_LOW)
-				count = 0;
-			else
-				count = 0xFF;
-		}
-		
+		delay_250ns();
+		delay_250ns();
+		delay_250ns();
+		delay_250ns();
 	}
+
+}
+
+void delay_milli(unsigned int ms)
+{
+	#ifdef SIMULATOR
+		ms = ms / 1000;
+		ms++;
+	#endif
+	// 1000 µs = 1 ms 
+	
+	delay_micro(ms * 1000);
+}
+
+// ------------------------------------------------------- KEYPAD ------------------------------------------------------------------------------- //
+
+void ActivateRow( unsigned int row )
+{
+
+    /* Aktivera angiven rad hos tangentbordet, eller
+    * deaktivera samtliga */
+    switch( row )
+    {
+    case 1: *GPIO_ODR_HIGH = 0x10; break;
+    case 2: *GPIO_ODR_HIGH = 0x20; break;
+    case 3: *GPIO_ODR_HIGH = 0x40; break;
+    case 4: *GPIO_ODR_HIGH = 0x80; break;
+    case 0: *GPIO_ODR_HIGH = 0x00; break;
+
+    }
+
+}
+
+int ReadColumn( void )
+{
+
+    /* Om någon tangent (i aktiverad rad)
+    * är nedtryckt, returnera dess kolumnnummer,
+    * annars, returnera 0 */
+    unsigned char c;
+    c = *GPIO_IDR_HIGH;
+    if ( c & 0x8 )
+        return 4;
+    if ( c & 0x4 )
+        return 3;
+    if ( c & 0x2 )
+        return 2;
+    if ( c & 0x1 )
+        return 1;
+
+    return 0;
+}
+
+unsigned char keyb(void)
+{
+
+    unsigned char key[]={1,2,3,0xA,4,5,6,0xB,7,8,9,0xC,0xE,0,0xF,0xD};
+
+    int row, col;
+    for(row=1; row <=4 ; row++ )
+    {
+        ActivateRow( row );
+        if( (col = ReadColumn () ) )
+        {
+            ActivateRow( 0 );
+            return key [4*(row-1)+(col-1) ];
+        }
+    }
+    ActivateRow( 0 );
+    return  0xFF;
+}
+
+// ------------------------------------------------------- SNAKE FUNCTIONS ------------------------------------------------------------------------------- //
+
+void apple_new(apple_t *apple, snake_t *snake) {
+    // Draw random positions until position not hitting snake found
+    bool ready = false;
+    int x_try, y_try;
+    while (!ready)
+    {
+        x_try = 1 + (char) *TIM6_CNT % (X_SIZE); // (char) *TIM6_CNT gets a random number 
+        y_try = 1 + (char) *TIM6_CNT % (Y_SIZE);
+        ready = true;
+        for (int i=0; i<snake->length && ready; ++i)
+        {
+            if (x_try == snake->body_part[0].posx && y_try == snake->body_part[0].posy)
+                ready = false;
+        }
+    }
+    apple->x = x_try;
+    apple->y = y_try;
+}
+
+void snake_turn(snake_t *snake) {
+	char input = keyb(); // fetch the key value to input
+	switch (input)
+	{
+	case 2:
+		snake->dir = UP;
+		break;
+	case 4:
+		snake->dir = LEFT;
+		break;
+	case 8:
+		snake->dir = DOWN;
+		break;
+	case 6:
+		snake->dir = RIGHT;
+		break;
+	default:
+		break;
+	}
+
+}
+
+void snake_move(snake_t *snake) 
+{
+    for (int i = snake->length - 0; i > 0; i--)
+    {
+        snake->body_part[i].posx = snake->body_part[i - 1].posx;
+        snake->body_part[i].posy = snake->body_part[i - 1].posy;
+        
+    }
+    switch (snake->dir)
+        {
+        case UP:
+            snake->body_part[0].posy--;
+            break;
+        case DOWN:
+            snake->body_part[0].posy++;
+            break;
+        case LEFT:
+            snake->body_part[0].posx--;
+            break;
+        default:
+            snake->body_part[0].posx++;
+            break;
+        }
+    
+}
+
+bool snake_eat_apple(apple_t *apple, snake_t *snake) {
+    if (snake->body_part[0].posx == apple->x && snake->body_part[0].posy == apple->y)
+    {
+        // Can't increase the length of the snake.
+        apple_new(apple, snake);
+        snake->length++;
+        int new = snake->length - 1;
+        
+        switch (snake->dir)
+        {
+        case UP:
+            snake->body_part[new].posx = snake->body_part[new - 1].posx;
+            snake->body_part[new].posy = snake->body_part[new - 1].posx + 1;
+        case DOWN:
+            snake->body_part[new].posx = snake->body_part[new - 1].posx;
+            snake->body_part[new].posy = snake->body_part[new - 1].posx - 1;
+        case LEFT:
+            snake->body_part[new].posx = snake->body_part[new - 1].posx - 1;
+            snake->body_part[new].posy = snake->body_part[new - 1].posx;
+            break;
+        default:
+            snake->body_part[new].posx = snake->body_part[new - 1].posx + 1;
+            snake->body_part[new].posx = snake->body_part[new - 1].posx;
+            break;
+        }
+
+        return true;
+    }
+    return false;
+}
+
+bool snake_hit_self(snake_t *snake) {
+    
+    if (snake->length > 2)
+    {
+        for (int i = 1; i < snake->length; i++)
+        {
+            if (snake->body_part[0].posx == snake->body_part[i].posx && snake->body_part[0].posy == snake->body_part[i].posy)
+            {
+                return true;
+            }
+        }
+        
+    }
+    return false;
+}
+
+bool snake_hit_wall(snake_t *snake) 
+{
+    for (int i = 0; i < X_SIZE; i++)
+    {
+        if (snake->body_part[0].posx == i && (snake->body_part[0].posy == 0 || snake->body_part[0].posy == Y_SIZE))
+        {
+            return true;
+        }
+    }
+    for (int j = 0; j < Y_SIZE; j++)
+    {
+        if (snake->body_part[0].posy == j && (snake->body_part[0].posx == 0 || snake->body_part[0].posx == X_SIZE)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// ------------------------------------------------------- GAME ------------------------------------------------------------------------------- //
+
+void main(void) 
+{
+	bool snake_dead = false;
+	snake_t snake;
+	init_app();
+
+	apple_t apple;
+    apple_new(&apple, &snake);
+
+	while (!snake_dead) 
+	{
+        snake_turn(&snake);
+        snake_move(&snake);
+		// need some kinda of draw function 
+    
+        if (snake_eat_apple(&apple, &snake)) 
+		{
+
+        }
+        snake_dead = snake_hit_wall(&snake) || snake_hit_self(&snake);
+        if (snake_dead) 
+		{
+            
+        }
+        
+    }
+	
+	
+
+
 }
